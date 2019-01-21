1e9ada52913643380d1ee3f3aa28fc436094e701
Tabish Tanseef <tabishtanseef@gmail.com>
2019-01-21 16:51:45 +0530

    funed First commit with most of the pages ready withount actual naming but only according to format

---
+++ js/main.js
@@ -0,0 +1,16 @@
+[].slice.call( document.querySelectorAll( 'button.progress-button' ) ).forEach( function( bttn ) {
+				new ProgressButton( bttn, {
+					callback : function( instance ) {
+						var progress = 0,
+							interval = setInterval( function() {
+								progress = Math.min( progress + Math.random() * 0.1, 1 );
+								instance._setProgress( progress );
+
+								if( progress === 1 ) {
+									instance._stop(1);
+									clearInterval( interval );
+								}
+							}, 200 );
+					}
+				} );
+			} );
